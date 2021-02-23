27a69cf929fafd6ddc4c43c02f24302a2e75d0f6
Mya Lee <69050459+MyaTLee@users.noreply.github.com>
2021-02-22 22:04:06 -0600

    DAY 2: JS

---
+++ battleship.js
@@ -0,0 +1,23 @@
+// Pseudocode:
+
+// Variables:
+
+
+//! What is happening?
+//? The Player sees a home screen with the options: Instructions and Start Game
+//? Start Game leads to choosing  the gameboard size (standard size: 8x8) and placing the ships on the board
+//? Save the ship placement to signify game start
+//? Text for Player to choose a spot to hit
+//? Image: hit (bomb/mushroom cloud) and miss (water wave)
+//? Computer turn & image
+
+// Inventory: ships for drag and drop
+
+// Set-up sequence: for the ships
+//can two opposing ships be in the same spots: yes both computer and player cna have ships in the same spot
+// placement: function that changes a li from empty to busy/filled to indicate that the ship in that spot
+
+//! need to look up how to do drag and drop function in js
+
+// at least two separate platforms/grids for each player: Player and Opponent and they both will track hits and misses
+//grid will be outlined with alphabets and numbers
